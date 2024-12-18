7532bc3cd419cbf7a5581f513e8eceab0c43e33a
Zsolt Varga <zs.zsolt.varga@gmail.com>
2024-12-18 00:21:29 +0000

    Day 16 p2 use modified dijkstra

--- 16/16.py
+++ 16/16.py
@@ -68,18 +68,19 @@ def number_of_turns(current_direction, target_direction):
 
     return min(left_turns, 4 - left_turns)
 
+def direction_to_target(current, target):
+    if target.x > current.x:
+        target_direction = Direction.RIGHT
+    elif target.x < current.x:
+        target_direction = Direction.LEFT
+    elif target.y < current.y:
+        target_direction = Direction.UP
+    elif target.y > current.y:
+        target_direction = Direction.DOWN
+    return target_direction
 
 def number_of_turns_to_position(current_direction, current_position, target_position):
-    target_direction = None
-    if target_position.x > current_position.x:
-        target_direction = Direction.RIGHT
-    elif target_position.x < current_position.x:
-        target_direction = Direction.LEFT
-    elif target_position.y < current_position.y:
-        target_direction = Direction.UP
-    elif target_position.y > current_position.y:
-        target_direction = Direction.DOWN
-
+    target_direction = direction_to_target(current_position, target_position)
     return (number_of_turns(current_direction, target_direction), target_direction)
 
 def get_neighbors(current_position):
@@ -103,12 +104,14 @@ def distance_and_direction(current_position, current_direction, target_position)
     # print(turns, new_direction)
     return ((turns * 1000) + 1, new_direction)
 
-def print_map():
+def print_map(override_marks = set()):
     for line_index, line in enumerate(map):
         string = []
         for column_index, tile in enumerate(line):
             if tile == wall:
                 string.append("#")
+            elif Position(column_index, line_index) in override_marks:
+                string.append("O")
             elif Position(column_index, line_index) == start:
                 string.append("S")
             elif Position(column_index, line_index) == goal:
@@ -161,10 +164,12 @@ def a_star():
     return None
 
 print("~~~~~~~~~~RESULT 1~~~~~~~~~~")
-print(f"start: {start}")
-print(f"end: {goal}")
-a_best_path_length = a_star()[0].cost_so_far
-print(a_best_path_length)
+# print(f"start: {start}")
+# print(f"end: {goal}")
+# a_best_path_length = a_star()[0].cost_so_far
+# print(a_best_path_length)
+
+# A* is super slow for this
 
 print("~~~~~~~~~~RESULT 2~~~~~~~~~~")
 def find_all_paths_of_length(position, previous_position, direction, remaining_length):
@@ -189,6 +194,92 @@ def find_all_paths_of_length(position, previous_position, direction, remaining_l
         result.append(position)
         result.extend(path)
     return result
+
+def create_node_id(position, direction):
+    return f"{position.x}-{position.y}-{direction}"
+
+def create_weighted_graph():
+    graph = {}
+    for line_index, line in enumerate(map):
+        for column_index, tile in enumerate(line):
+            if tile == wall:
+                continue
+            position = Position(column_index, line_index)
+            neighbors = get_neighbors(position)
+            arrival_directions = [direction_to_target(n, position) for n in neighbors]
+
+            if Position(column_index, line_index) == start:
+                arrival_directions.append(Direction.RIGHT)
+
+            for arrival_direction in arrival_directions:
+                def neighbor_tuple(neighbor_position):
+                    weight, direction = distance_and_direction(position, arrival_direction, neighbor_position)
+                    neighbor_id = create_node_id(neighbor_position, direction)
+                    return (neighbor_id, weight)
+
+                node_id = create_node_id(position, arrival_direction)
+                graph[node_id] = [neighbor_tuple(n) for n in neighbors]
+    return graph
+
+def dijkstra():
+    graph = create_weighted_graph()
+
+    source = create_node_id(start, start_direction)
+
+    distances = {node: float('inf') for node in graph}
+    distances[source] = 0
+    came_from = {node: [] for node in graph}
+    queue = [(0, source)]
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        # Skip if we've already found a better path
+        if current_distance > distances[current_node]:
+            continue
+
+        # Explore neighbors
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+
+            # Update if a shorter path is found
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                came_from[neighbor] = [current_node]
+                heapq.heappush(queue, (distance, neighbor))
+
+            elif distance == distances[neighbor]:
+                came_from[neighbor].append(current_node)
+
+    return distances, came_from
+
+def id_to_position(id):
+    id_arr = id.split("-")
+    return Position(int(id_arr[0]), int(id_arr[1]))
+
+def dijkstra_came_from_tree(came_from, target):
+    children = came_from[target]
+    if len(children) == 0:
+        return None
+    prev = [x for x in [dijkstra_came_from_tree(came_from, child) for child in children] if x is not None]
+    return {"pos": id_to_position(target), "prev": prev}
+
+def add_tree_positions(tree, set):
+    set.add(tree["pos"])
+    for branch in tree.get("prev", []):
+        add_tree_positions(branch, set)
+
+dijkstra = dijkstra()
+goal_id = create_node_id(goal, Direction.UP).replace(f"{Direction.UP}", "")
+goal_distances_key_value = [(k, dijkstra[0][k]) for k in dijkstra[0].keys() if goal_id in k]
+solution_key_value = min(goal_distances_key_value, key = lambda kv: kv[1])
+print(solution_key_value)
+
+tree = dijkstra_came_from_tree(dijkstra[1], solution_key_value[0])
+all_positions = set()
+add_tree_positions(dijkstra_came_from_tree(dijkstra[1], solution_key_value[0]), all_positions)
+# print_map(all_positions)
+print(len(all_positions)+1)
 
 # Save timestamp
 end_time = time.time()
