afe8faa6607729b6f362cf0d250bf123ee91b9de
Zsolt Varga <zs.zsolt.varga@gmail.com>
2024-12-24 23:40:57 +0100

    Day 23 p2

--- 23/23.py
+++ 23/23.py
@@ -61,6 +61,53 @@ def find_groups():
 
 print(len(find_groups()))
 
+print("~~~~~~~~~~RESULT 2~~~~~~~~~~")
+
+def is_part_of_clique(clique, node):
+    for c in clique:
+        if c not in graph[node]:
+            return False
+    return True
+
+def expand_clique(clique_so_far, evaluated):
+    all_neighbors = []
+    for node in clique_so_far:
+        all_neighbors += [n for n in graph[node] if n not in evaluated and n not in clique_so_far]
+    if not all_neighbors:
+        return clique_so_far
+
+    start_size = len(clique_so_far)
+    new_clique_so_far = clique_so_far
+    for neighbor in all_neighbors:
+        if is_part_of_clique(new_clique_so_far, neighbor):
+            new_clique_so_far.append(neighbor)
+        evaluated.add(neighbor)
+
+    if len(new_clique_so_far) > start_size:
+        return expand_clique(new_clique_so_far, evaluated)
+    else:
+        return clique_so_far
+
+
+def find_largest_clique():
+    queue = list(graph.keys())
+    known_cliques = []
+    largest_clique = []
+    while queue:
+        current_node = queue[0]
+        evaluated_nodes = [n for c in known_cliques for n in c]
+
+        new_clique = expand_clique([current_node], set(evaluated_nodes))
+        known_cliques.append(new_clique)
+        if len(new_clique) > len(largest_clique):
+            largest_clique = new_clique
+        # Assumes all nodes are only part of one clique
+        for node in new_clique:
+            queue.remove(node)
+
+    return largest_clique
+
+print(",".join(sorted(find_largest_clique())))
 
 # Save timestamp
 end_time = time.time()
